6e5a398f4a2352a485b27a06a8d985a09129cab2
1405281646 <1405281646@qq.com>
2022-11-02 11:54:26 +0800

    Initial commit

---
+++ src/main/java/com/zkl/i_learn/ILearnApplication.java
@@ -0,0 +1,13 @@
+package com.zkl.i_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILearnApplication.class, args);
+    }
+
+}
